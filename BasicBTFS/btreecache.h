fb9d734e5d86e02bf8349197e782adffab11733c
QuardellV <qverbond@gmail.com>
2022-08-05 00:27:17 +0200

    init file for btree cache

---
+++ BasicBTFS/btreecache.h
@@ -0,0 +1,13 @@
+#ifndef BASICBTFS_BTREECACHE_H
+#define BASICBTFS_BTREECACHE_H
+
+#include <linux/buffer_head.h>
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include "basicbtfs.h"
+#include "bitmap.h"
+#include "cache.h"
+
+#endif
