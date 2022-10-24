02532fdcbc9abd19ef05f1ad691f782f181c8204
QuardellV <qverbond@gmail.com>
2022-10-25 01:14:39 +0200

    rename benchmark names

---
+++ fs_benchmark.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#!/bin/bash
+
+cd BasicBTFS
+
+./clean.sh && ./benchmark_btfs.sh && ./clean.sh
+
+cd ../BasicBTFSNC
+
+./clean.sh && ./benchmark_btfsnc.sh && ./clean.sh
+
+cd ../BasicFATFS
+
+./clean.sh && ./benchmark_ftfs.sh && ./clean.sh
+
+cd ../BTRFS
+
+./clean.sh && ./benchmark_btrfs.sh && ./clean.sh
+
+cd ../FATFS
+
+./clean.sh && ./benchmark_fat.sh && ./clean.sh
+
+cd ..
